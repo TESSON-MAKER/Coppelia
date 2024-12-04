c0768df2e6882b5b94364647732622313e3c19e9
Paul TESSON <paul.tesson.officiel@gmail.com>
2024-12-04 21:08:28 +0100

    Update

--- UC2/control_Pioneer_avec_capteur-IR.py
+++ UC2/control_Pioneer_avec_capteur-IR.py
@@ -1,82 +1,89 @@
 import vrep
 import math
-import time
 import numpy as np
 
-
+# Conversion functions
 def to_rad(deg):
-    return 2*math.pi*deg/360
+    return 2 * math.pi * deg / 360
 
 def to_deg(rad):
-    return rad*360/(2*math.pi)
+    return rad * 360 / (2 * math.pi)
 
+# Simulation configuration
+IP = '127.0.0.1'
+PORT = 19997
+SCENE = './Pioneer_murs.ttt'
+POSITION_INIT = [3, 2, to_rad(0)]
 
-# simulation config
-ip = '127.0.0.1'
-port = 19997
-scene = './Pioneer_murs.ttt'
-position_init = [3,2,to_rad(0)]
+print('Program started')
+vrep.simxFinish(-1)  # Close all opened connections
+client_id = vrep.simxStart(IP, PORT, True, True, 5000, 5)  # Connect to V-REP
 
+if client_id != -1:
+    print(f'Connected to remote API server on {IP}:{PORT}')
 
-print ('Program started')
-vrep.simxFinish(-1) # just in case, close all opened connections
-client_id=vrep.simxStart(ip,port,True,True,5000,5) # Connect to V-REP
+    # Load the scene
+    res = vrep.simxLoadScene(client_id, SCENE, 1, vrep.simx_opmode_oneshot_wait)
 
-if client_id!=-1:
-    print ('Connected to remote API server on %s:%s' % (ip, port))
-    res = vrep.simxLoadScene(client_id, scene, 1, vrep.simx_opmode_oneshot_wait)
+    # Get handles for robot and motors
     res, pioneer = vrep.simxGetObjectHandle(client_id, 'Pioneer_p3dx', vrep.simx_opmode_oneshot_wait)
     res, left_motor = vrep.simxGetObjectHandle(client_id, 'Pioneer_p3dx_leftMotor', vrep.simx_opmode_oneshot_wait)
     res, right_motor = vrep.simxGetObjectHandle(client_id, 'Pioneer_p3dx_rightMotor', vrep.simx_opmode_oneshot_wait)
-    
 
-    # For Sensors
+    # Initialize sensor handles and detection arrays
     sensor_handles = np.zeros(16)
     detection_IR = np.zeros(16)
 
-    # Reading data for sensors
+    # Get sensor handles and start streaming proximity sensor data
     for i in range(1, 17):
         res, sensor_handle = vrep.simxGetObjectHandle(client_id, f"Pioneer_p3dx_ultrasonicSensor{i}", vrep.simx_opmode_blocking)
-        sensor_handles[i-1] = sensor_handle
-        res, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(client_id, sensor_handle, vrep.simx_opmode_streaming)
+        sensor_handles[i - 1] = sensor_handle
+        res, _, _, _, _ = vrep.simxReadProximitySensor(client_id, sensor_handle, vrep.simx_opmode_streaming)
 
+    # Start the simulation
     vrep.simxStartSimulation(client_id, vrep.simx_opmode_oneshot_wait)
 
-    
-    v0 = 2.0
-    noDetectionDist = 0.5
-    maxDetectionDist = 0.2
+    # Braitenberg parameters
+    V0 = 2.0
+    NO_DETECTION_DIST = 0.5
+    MAX_DETECTION_DIST = 0.2
     detect = [0] * 16
-    braitenbergL = [-0.2, -0.4, -0.6, -0.8, -1, -1.2, -1.4, -1.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    braitenbergR = [-1.6, -1.4, -1.2, -1, -0.8, -0.6, -0.4, -0.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    BRAITENBERG_L = [-0.2, -0.4, -0.6, -0.8, -1, -1.2, -1.4, -1.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    BRAITENBERG_R = [-1.6, -1.4, -1.2, -1, -0.8, -0.6, -0.4, -0.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
-
-    while True:
-        for i in range(16):
-            res, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(client_id, int(sensor_handles[i]), vrep.simx_opmode_buffer)
-            if detectionState:
-                distToObject = math.sqrt(detectedPoint[0] ** 2 + detectedPoint[1] ** 2 + detectedPoint[2] ** 2)
-                if distToObject < noDetectionDist:
-                    detect[i] = 1 - ((distToObject - maxDetectionDist) / (noDetectionDist - maxDetectionDist))
-                    detect[i] = max(detect[i], 0)
+    try:
+        while True:
+            # Update detection states and compute activation levels
+            for i in range(16):
+                res, detection_state, detected_point, _, _ = vrep.simxReadProximitySensor(client_id, int(sensor_handles[i]), vrep.simx_opmode_buffer)
+                if detection_state:
+                    dist_to_object = math.sqrt(detected_point[0] ** 2 + detected_point[1] ** 2 + detected_point[2] ** 2)
+                    if dist_to_object < NO_DETECTION_DIST:
+                        detect[i] = 1 - ((dist_to_object - MAX_DETECTION_DIST) / (NO_DETECTION_DIST - MAX_DETECTION_DIST))
+                        detect[i] = max(detect[i], 0)
+                    else:
+                        detect[i] = 0
                 else:
                     detect[i] = 0
-            else:
-                detect[i] = 0
-        
-        vLeft = v0
-        vRight = v0
-        for i in range(16):
-            vLeft += braitenbergL[i] * detect[i]
-            vRight += braitenbergR[i] * detect[i]
 
-        # Apply motor velocities
-        vrep.simxSetJointTargetVelocity(client_id, left_motor, vLeft, vrep.simx_opmode_oneshot)
-        vrep.simxSetJointTargetVelocity(client_id, right_motor, vRight, vrep.simx_opmode_oneshot)
+            # Compute motor velocities
+            v_left = V0
+            v_right = V0
+            for i in range(16):
+                v_left += BRAITENBERG_L[i] * detect[i]
+                v_right += BRAITENBERG_R[i] * detect[i]
 
-    # Terminate
+            # Apply motor velocities
+            vrep.simxSetJointTargetVelocity(client_id, left_motor, v_left, vrep.simx_opmode_oneshot)
+            vrep.simxSetJointTargetVelocity(client_id, right_motor, v_right, vrep.simx_opmode_oneshot)
+
+    except KeyboardInterrupt:
+        print("Simulation stopped by user")
+
+    # Stop the simulation
     vrep.simxStopSimulation(client_id, vrep.simx_opmode_oneshot_wait)
     vrep.simxFinish(client_id)
+    print("Simulation finished")
 
 else:
-    print('Unable to connect to %s:%s' % (ip, port))
+    print(f'Unable to connect to {IP}:{PORT}')
